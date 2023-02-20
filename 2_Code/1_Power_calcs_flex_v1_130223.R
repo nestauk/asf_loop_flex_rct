b5b5d9863eeec8d12696bff22a656af148d3acb2
Oli-Berry <85549988+Oli-Berry@users.noreply.github.com>
2023-02-20 10:38:59 +0000

    Minor changes before review

--- 2_Code/1_Power_calcs_flex_v1_130223.R
+++ 2_Code/1_Power_calcs_flex_v1_130223.R
@@ -11,10 +11,9 @@ library(stringr) #------- String operations (`str_wrap`)
 library(purrr) #--------- Vectorization
 library(data.table) #---- For transposing
 library(readr) #--------- Reading csv files
-library(readxl) #-------- Reading Excel files
 library(summarytools) #-- Descriptive statistics
 library(jtools) #-------- Regression outputs
-library(lmtest) #-------- Robust SEs
+library(lmtest) #-------- Linear regression tests
 library(sandwich) #------ Robust SEs
 
 # Data pathways
@@ -25,6 +24,7 @@ out_dir <- file.path(bas_dir, "3_Outputs")
 # Set seed
 set.seed(20230103)
 start_time <- Sys.time()
+
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -54,7 +54,7 @@ str(data)
 
 # Creating opt-out variable
 data <- data |>
-  mutate( opt_in = case_when(
+  mutate(opt_in = case_when(
     took_part == "TRUE" ~ 1,
     took_part == "FALSE" ~ 0,
     TRUE ~ 99
@@ -70,17 +70,16 @@ data <- data |>
 
 # ~~ I. Usage ~~ #
 
-#~~~ Summary statistics
+# Summary statistics
 summary(data$usage)
 descr(data$usage)
 nrow(data) ## 14,616 rows
 length(data$usage[data$usage == 0]) ## 348 zeros (roughly 2%)
 
-#~~~ Graphing
+# Graphing
 hist(data$usage, breaks = 1e3) ## Looks very skewed, with a lot of zeros
 
 hist(log(data$usage[data$usage != 0]), breaks = 1e3) ## Looks more normal
-hist(log(data$usage) +1, breaks = 1e3)
 
 
 # ~~ II. Opt ins ~~ #
@@ -115,31 +114,37 @@ for (i in 1:sims) {
   
   #~~~ Selecting sample size
   ## We create a new dataframe so we don't need to import the data each time.
-
-  data_to_use <- data |>
-    filter(!duplicated(id)) |> ## Removing duplicates
-     mutate(rand = runif(1)) |> ## Generating random number
-      arrange(rand) ## Sorting by random number
   
-  # Subsetting to correct sample size
+  # Creating random number and ordering
+  data_to_use <- data |>
+    filter(!duplicated(id)) |> ## Removing duplicates (there aren't any fortunately)
+    mutate(rand = runif(1)) |> ## Generating random number
+    arrange(rand) ## Sorting by random number
+  
+  # Selecting sample for this simulation
   sample <- sample_seq[(sim %% (length(sample_seq)) +1)] ## This is a way to subsequently sample from sample_seq
   
+  # Subsetting to correct sample size
   data_to_use <- data_to_use |>
     filter(row_number() <= sample)
-
+  
   #~~~ Random treatment assignment
+  ## We are using simple randomisation
+  
   data_to_use <- data_to_use |>
     group_by(id) |>
-      mutate(treat_num = sample(2, 1, replace = TRUE)) |>
-        ungroup() |>
-          mutate(treatment = case_when(
-            treat_num == 1 ~ "Control",
-            treat_num == 2 ~ "Treatment",
-            TRUE ~ "Missing"
-          ))
+    mutate(treat_num = sample(2, 1, replace = TRUE)) |>
+    ungroup() |>
+    mutate(treatment = case_when(
+      treat_num == 1 ~ "Control",
+      treat_num == 2 ~ "Treatment",
+      TRUE ~ "Missing"
+    ))
   
-
+  
   # ~~ II. Running regression ~~ #
+  ## We want to assess power for three different regression specifications
+  ## This uses a different model for each third of simulations
   
   if (sim <= length(sample_seq) * 10) {
     
@@ -150,8 +155,8 @@ for (i in 1:sims) {
     
     # Robust SEs
     fit_coef <- coeftest(fit, vcov. = vcovHC, type = "HC3")
-  
-  
+    
+    
   } else if (sim <= length(sample_seq) * 20) {
     
     type <- "Logged"
@@ -164,14 +169,14 @@ for (i in 1:sims) {
     
     # Robust SEs
     fit_coef <- coeftest(fit, vcov. = vcovHC, type = "HC3")
-  
+    
     
   } else {
     
     type <- "Poisson"
     
     # Fit model
-    fit <- glm(usage ~ treatment, family= "poisson", data = data_to_use)
+    fit <- glm(usage ~ treatment, family = "poisson", data = data_to_use)
     
     # Robust SEs
     fit_coef <- coeftest(fit, vcov. = vcovHC, type = "HC3")
@@ -186,7 +191,7 @@ for (i in 1:sims) {
   # Control statistics
   control_mean <- mean(data_to_use$usage[data_to_use$treatment == "Control"], na.rm = TRUE)
   control_sd <- sd(data_to_use$usage[data_to_use$treatment == "Control"], na.rm = TRUE)
-
+  
   
   # ~~ IV. Aggregating ~~ #
   
@@ -205,7 +210,7 @@ for (i in 1:sims) {
     fit_simple <- lm(usage ~ treatment, data = data_to_use)
     print(summ(fit_simple))
     print(summ(fit_simple, robust = "HC3"))
-
+    
     sink()
     
   } else {
@@ -220,6 +225,7 @@ write.csv(output_data, file = paste(out_dir,"flex_power.csv", sep = "/"))
 
 
 # ~~ III. Summarising for specific numbers of Treatment arms ~~ #
+## This is for a pretty graph - it could be done manually in Excel, but this is more fun.
 
 #~~~ Setting up arms
 output_data <- output_data |>
@@ -229,19 +235,19 @@ output_data <- output_data |>
     sample == 7000 ~ 4, ## ~3,500 in each arm, so 7,000 for T/C comparison
     sample == 5500 ~ 5, ##  ~2,8000 in each arm, so ~5,500 for T/C comparison
     TRUE ~ 99
-    )
+  )
   )
 
 # Summarising
 output_table <- output_data |>
   group_by(type, sample) |>
-    summarise(
-      control_mean = mean(control_mean, na.rm = TRUE),
-      control_sd = mean(control_mean, na.rm = TRUE),
-      beta = mean(beta, na.rm = TRUE),
-      se = mean(se, na.rm = TRUE),
-      arms = mean(arms, na.rm = TRUE)
-    )
+  summarise(
+    control_mean = mean(control_mean, na.rm = TRUE),
+    control_sd = mean(control_mean, na.rm = TRUE),
+    beta = mean(beta, na.rm = TRUE),
+    se = mean(se, na.rm = TRUE),
+    arms = mean(arms, na.rm = TRUE)
+  )
 
 # Filtering for experimental arms
 output_table <- output_table |>
@@ -249,15 +255,17 @@ output_table <- output_table |>
 
 # Adjusting alpha for multiple comparisons (Bonferroni)
 output_table$alpha_adj <-  0.05 / (output_table$arms - 1) 
-  
-## Rule of 2.8, two-tailed
+
+# Rule of 2.8, two-tailed
 output_table$mde <-  output_table$se * (qnorm(0.8) + qnorm (1 - output_table$alpha_adj /2 ))
 
-## Calculating % change
+# Calculating % change
+## Log and Poisson MDE are already expressed as % changes - so this is just for OLS
+
 output_table$mde_prop <- 0
 output_table$mde_prop[output_table$type == "OLS"] <- output_table$mde[output_table$type == "OLS"] / output_table$control_mean[output_table$type == "OLS"]
 output_table$mde_prop[output_table$type != "OLS"] <- output_table$mde[output_table$type != "OLS"]
-  
+
 
 #~~~ Export
 write.csv(output_table, file = paste(out_dir,"flex_power_arms.csv", sep = "/"))
